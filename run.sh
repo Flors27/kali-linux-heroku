22b2ffba141cee772fd8f0d4239a6ec4b23f4008
Flors27 <dorinflorinnicolae@yahoo.com>
2024-10-22 04:10:15 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Instalación de Kali Linux dentro del contenedor Heroku
-apt-get update && apt-get install -y kali-linux-headless
-
-# Comando para ejecutar la shell de Kali
-echo "Kali Linux instalado. Iniciando bash..."
-bash
